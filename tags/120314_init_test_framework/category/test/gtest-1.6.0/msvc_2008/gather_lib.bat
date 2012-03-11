38a4233593a3377268ef1386e02b487f46600754
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-03-11 04:45:27 +0000

    Tag: 최초 구성 1차완료(/trunk)

---
+++ tags/120314_init_test_framework/category/test/gtest-1.6.0/msvc_2008/gather_lib.bat
@@ -0,0 +1,16 @@
+@echo off
+ECHO Clean lib
+rd /s /q lib
+md lib
+rem
+ECHO Copy /MD to lib
+copy gtest-md\Debug\gtest_main-mdd.lib	lib\gtest_main-mdd.lib
+copy gtest-md\Release\gtest_main-md.lib lib\gtest_main-md.lib
+copy gtest-md\Debug\gtestd.lib		lib\gtest_mdd.lib
+copy gtest-md\Release\gtest.lib		lib\gtest_md.lib
+rem
+ECHO Copy /MT to lib
+copy gtest\Debug\gtest_maind.lib	lib\gtest_main-mtd.lib
+copy gtest\Release\gtest_main.lib	lib\gtest_main-mt.lib
+copy gtest\Debug\gtestd.lib		lib\gtest_mtd.lib
+copy gtest\Release\gtest.lib		lib\gtest_mt.lib
