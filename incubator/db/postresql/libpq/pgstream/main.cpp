062e979ba6577a41560af370cfc4f6fea7be5b5f
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-03-12 06:27:59 +0000

    [db:PostgreSQL] The pgstream library - pgstream-1.0-beta.tar.gz (Error: 13개, Warning: 29개)

---
+++ incubator/db/postresql/libpq/pgstream/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "pgstream.h"
+
+int main() {
+	pg_cnx cnx;
+	std::string subject;
+	try {
+		cnx.connect("dbname=mail host=localhost port=5433");
+		cnx.set_option("bind_variables", false);
+		cnx.set_option("nested_transactions", true);
+		cnx.set_option("prepare_statements", false);
+		pg_stream s("SELECT IP_ADDRESS FROM DEVICE", cnx);
+		while(!s.eos()) {
+			char ip[48+1];
+			s >> ip;
+			std::cout << ip << std::endl;
+		}
+	}
+	catch(pg_excpt p) {
+		if (!p.query().empty())
+			std::cerr << p.query() << "\n";
+		std::cerr << p.errmsg();
+		if (!p.errcode().empty()) {
+			std::cerr << "(ERR=" << p.errcode() << ")" << std::endl;
+		}
+	}
+
+	return 0;
+}
