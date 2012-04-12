43e0cb2078e430953ac7701b9459134abe499c05
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-04-12 09:54:09 +0000

    웹서비스 인터페이스 구현

--- trunk/incubator/net/WhoConnectedRemote/wcr_console/Program.cs
+++ trunk/incubator/net/WhoConnectedRemote/wcr_console/Program.cs
@@ -9,7 +9,17 @@ namespace wcr_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RemoteDesktopFinder.WhoWithHtml());
+            //Console.WriteLine(RemoteDesktopFinder.WhoWithHtml());
+            WebServer ws = new WebServer();
+            Console.WriteLine("Start the service");
+            ws.Start();
+
+            // 대기
+            Console.WriteLine("Press Any key to stop the service");
+            Console.ReadKey(true);
+
+            Console.WriteLine("Stop the service");
+            ws.Stop();
         }
     }
 }
---
+++ trunk/incubator/net/WhoConnectedRemote/wcr_console/WebServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace wcr_console
+{
+    public class WebServer
+    {
+        private static string Prefix = "http://*:8282/";
+        // refs. http://msdn.microsoft.com/en-us/library/system.net.httplistener(v=vs.90).aspx
+        private HttpListener m_listener;
+
+        public void Start()
+        {
+            if (!HttpListener.IsSupported)
+            {
+                throw new Exception("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+            }
+
+            m_listener = new HttpListener();
+            m_listener.Prefixes.Add(Prefix);
+            m_listener.Start();
+            m_listener.BeginGetContext(new AsyncCallback(ListenerCallback), m_listener);
+        }
+
+        public void Stop()
+        {
+            m_listener.Close();
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            HttpListener listener = (HttpListener)result.AsyncState;
+            // Call EndGetContext to complete the asynchronous operation.
+            HttpListenerContext context = listener.EndGetContext(result);
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(RemoteDesktopFinder.WhoWithHtml());
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+
+            m_listener.BeginGetContext(new AsyncCallback(ListenerCallback), m_listener);
+        }
+    }
+}
