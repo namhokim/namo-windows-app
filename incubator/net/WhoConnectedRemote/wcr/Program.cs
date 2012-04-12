fd3654a2592272e8a5e69d6f4fd291872c4599ce
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-04-12 12:18:18 +0000

    콘솔에서 작성한 클래스 참조 및 웹서비스 시작/중지 추가

--- incubator/net/WhoConnectedRemote/wcr/Program.cs
+++ incubator/net/WhoConnectedRemote/wcr/Program.cs
@@ -16,7 +16,7 @@ namespace wcr
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[] 
 			{ 
-				new Service1() 
+				new WebService() 
 			};
             ServiceBase.Run(ServicesToRun);
         }
--- incubator/net/WhoConnectedRemote/wcr/WebService.cs
+++ incubator/net/WhoConnectedRemote/wcr/WebService.cs
@@ -6,22 +6,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using wcr_console;
 
 namespace wcr
 {
-    public partial class Service1 : ServiceBase
+    public partial class WebService : ServiceBase
     {
-        public Service1()
+        private WebServer m_ws;
+
+        public WebService()
         {
             InitializeComponent();
+            m_ws = new WebServer();
         }
 
         protected override void OnStart(string[] args)
         {
+            m_ws.Start();
         }
 
         protected override void OnStop()
         {
+            m_ws.Stop();
         }
     }
 }
