79cebfce2664dc3b2317b38eb56b88ca6d0a738d
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-04-16 07:46:17 +0000

    add arp handler

--- incubator/nodejs/who_desktop/index.js
+++ incubator/nodejs/who_desktop/index.js
@@ -5,5 +5,6 @@ var requestHandlers = require("./requestHandlers");
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/favicon.ico"] = requestHandlers.favicon;
+handle["/arp"] = requestHandlers.arp;
 
 server.start(router.route, handle);
--- incubator/nodejs/who_desktop/requestHandlers.js
+++ incubator/nodejs/who_desktop/requestHandlers.js
@@ -2,7 +2,8 @@ var MONITOR_PORT = 3389;
 
 var fs = require("fs"),
     spawn = require('child_process').spawn,
-    jade = require('jade');
+    jade = require('jade'),
+	url = require('url');
 
 var jadefile = fs.readFileSync(__dirname + '/start.jade');
 var fn = jade.compile(jadefile.toString('utf8'));
@@ -16,7 +17,7 @@ function start(response) {
     
     cspr.stdout.setEncoding('utf8');
     cspr.stdout.on('data', function(data) {
-		console.log("whoConnect");
+		console.log(bin);
         var str = data.toString(), lines = str.split(",");
         var html = fn({"lines":lines});
         response.writeHead(200, {"Content-Type": "text/html"});
@@ -49,6 +50,37 @@ function favicon(response) {
   });
 }
 
+// http://www.shinstudio.com/blog/programming/pulling-query-string-in-node-js/
+function arp(response, request) {
+	var urlObj = url.parse(request.url, true);
+	var q = urlObj.query["q"];
+	console.log("Request handler 'arp' was called.");
+
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	if(typeof q != "undefined") {
+		console.log(" query : " + q);
+		var bin = "SendARP.exe";
+		var args = [q];
+		var cspr = spawn(bin, args);
+		cspr.stdout.setEncoding('utf8');
+		cspr.stdout.on('data', function(data) {
+			response.write(data);
+			response.end();
+		});
+		cspr.stderr.on('data', function(data) {
+			response.write(data);
+			response.end();
+		});
+		cspr.on('exit', function (data) {
+			response.write('NO DATA');
+			response.end();
+		});
+	} else {
+		response.write("NO PARAMETER(?q=xxx)");
+		response.end();
+	}
+}
 
 exports.start = start;
 exports.favicon = favicon;
+exports.arp = arp;
