15297f825d40b437ff32d349338464a9632c889e
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-05-08 09:38:43 +0000

    [nodejs:native] refactoring for native module call

--- trunk/incubator/nodejs/who_desktop/vsproj/src/whoConnect.cpp
+++ trunk/incubator/nodejs/who_desktop/vsproj/src/whoConnect.cpp
@@ -3,22 +3,21 @@
 #include <tchar.h>		// for _tcscat_s, _tcschr, _tcslen
 #include <Wtsapi32.h>	// for WTSEnumerateSessions, WTSQuerySessionInformationA
 #include <Ws2def.h>		// for AF_INET
-#include <stdio.h>		// for fprint, stdout, stderr
-#include <stdlib.h>		// for exit
+#include <stdlib.h>		// for EXIT_SUCCESS, EXIT_FAILURE
 #include <string>		// for std::string
 #include <sstream>		// for std::stringstream
 #include "json/json.h"
 
 #pragma comment(lib, "Wtsapi32.lib")
 
-const int ArgNone	= 1;
-const int ArgTwo	= 3;
-const char* JSON_NO_DATA = "{number:0,data:null}";
+///////////////////////////////////////////////////////////////////////////////
 
-int ShowCurrentConnectedUser();
+const char* JSON_NO_DATA = "{\"number\":0,\"data\":null}";
+
+///////////////////////////////////////////////////////////////////////////////
+
 void GetDetailInfo(DWORD SessionId, Json::Value& item_out);
 void GetAddressInfo(PWTS_CLIENT_ADDRESS address, std::string& address_str);
-int SendMessageToSesstionID(DWORD SessionId, LPCWSTR message);
 int GetMessageTitle(LPWSTR title);
 
 namespace convert {
@@ -26,24 +25,10 @@ namespace convert {
 	std::string W2UTF8(const std::wstring& utf16le);
 }
 
-int wmain(int argc, wchar_t *argv[])
-{
-	switch(argc) {
-		case ArgTwo:
-			{
-				DWORD sessionId = _wtoi(argv[1]);
-				std::wstring msg( argv[2] );
-				return SendMessageToSesstionID(sessionId, msg.c_str());
-			}
-		case ArgNone:
-		default:
-			return ShowCurrentConnectedUser();
-	}
+///////////////////////////////////////////////////////////////////////////////
 
-	return EXIT_SUCCESS;
-};
-
-int ShowCurrentConnectedUser()
+// inner call GetDetailInfo -> GetAddressInfo
+int ShowCurrentConnectedUser(Json::Value& out)
 {
 	PWTS_SESSION_INFOW pSes;
 	DWORD dwCount, i, c;
@@ -54,7 +39,8 @@ int ShowCurrentConnectedUser()
 	// refs. http://msdn.microsoft.com/en-us/library/aa383833(v=vs.85).aspx
 	bRes = WTSEnumerateSessionsW(WTS_CURRENT_SERVER_HANDLE, 0, 1, &pSes, &dwCount);
 	if(0==bRes) {	// if error
-		fprintf(stdout, JSON_NO_DATA);
+		Json::Reader r;
+		r.parse(JSON_NO_DATA, out);
 		return EXIT_FAILURE;
 	}	
 
@@ -72,14 +58,37 @@ int ShowCurrentConnectedUser()
 	}
 	WTSFreeMemory(pSes);
 
-	Json::Value root;
-	root["number"] = (unsigned int)c;
-	root["data"] = data;
+	// Set result
+	out["number"] = (unsigned int)c;
+	out["data"] = data;
 
-	Json::FastWriter w;
-	printf(w.write(root).c_str());
 	return EXIT_SUCCESS;
 }
+
+// inner call : GetMessageTitle
+int SendMessageToSesstionID(unsigned int SessionId, const wchar_t* message)
+{
+	DWORD dwTimeout = 60;	// 60 seconds
+	DWORD dwTimeoutIndefinitely = 0;	// 
+	DWORD Response = 0;
+
+	WCHAR szTitle[1024] = {0};
+	WCHAR szMessage[1024] = {0};
+	GetMessageTitle(szTitle);
+	wsprintfW(szMessage, message);
+
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/aa383842(v=vs.85).aspx
+	BOOL bRes = WTSSendMessage(
+		WTS_CURRENT_SERVER_HANDLE,
+		SessionId,
+		szTitle, static_cast<DWORD>(wcslen(szTitle)*sizeof(WCHAR)),
+		szMessage, static_cast<DWORD>(wcslen(szMessage)*sizeof(WCHAR)),
+		MB_OK, dwTimeoutIndefinitely, &Response, FALSE);
+
+	return (0!=bRes);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 void GetDetailInfo(DWORD SessionId, Json::Value& item_out)
 {
@@ -137,27 +146,7 @@ void GetAddressInfo(PWTS_CLIENT_ADDRESS address, std::string& address_str)
 	}
 }
 
-int SendMessageToSesstionID(DWORD SessionId, LPCWSTR message)
-{
-	DWORD dwTimeout = 60;	// 60 seconds
-	DWORD dwTimeoutIndefinitely = 0;	// 
-	DWORD Response = 0;
-
-	WCHAR szTitle[1024] = {0};
-	WCHAR szMessage[1024] = {0};
-	GetMessageTitle(szTitle);
-	wsprintfW(szMessage, message);
-
-	// http://msdn.microsoft.com/en-us/library/windows/desktop/aa383842(v=vs.85).aspx
-	BOOL bRes = WTSSendMessage(
-		WTS_CURRENT_SERVER_HANDLE,
-		SessionId,
-		szTitle, static_cast<DWORD>(wcslen(szTitle)*sizeof(WCHAR)),
-		szMessage, static_cast<DWORD>(wcslen(szMessage)*sizeof(WCHAR)),
-		MB_OK, dwTimeoutIndefinitely, &Response, FALSE);
-
-	return (0!=bRes);
-}
+///////////////////////////////////////////////////////////////////////////////
 
 int GetMessageTitle(LPWSTR title)
 {
@@ -180,6 +169,8 @@ int GetMessageTitle(LPWSTR title)
 	return wsprintfW(title, MessageFormat, szTime);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// UNICODE converter : UTF16LE <-> UTF8
 namespace convert {
 	const wchar_t Pow2_6 = 64;
 	const wchar_t Pow2_12 = 4096;
