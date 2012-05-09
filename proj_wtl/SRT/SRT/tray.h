5b42c33cf1f90e6758aa5f416f9df22e071391af
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-05-09 10:37:42 +0000

    Before adapt to V8 addons binaries.

--- proj_wtl/SRT/SRT/tray.h
+++
@@ -1,6 +0,0 @@
-#define TRAY_NOTIFY		WM_USER+1	// 사용자 정의 메시지
-
-#ifndef _WINDOWS_		// @@@ 수정(2008/02/14, NHKim) @@@ windows.h 추가 정의 방지
-#include <windows.h>
-#endif
-void SetTray(HWND hWindowHanlde, HICON hIconHandle, int message, LPCTSTR tip=NULL);
