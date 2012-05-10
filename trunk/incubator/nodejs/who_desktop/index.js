f6bb7aad3f1dfcb707ea7958298f1fdd69e20403
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-05-10 15:13:02 +0000

    [nodejs::who_desktop] jQuery Self hosting & semi colon fix

--- trunk/incubator/nodejs/who_desktop/index.js
+++ trunk/incubator/nodejs/who_desktop/index.js
@@ -2,7 +2,7 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-var handle = {}
+var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/favicon.ico"] = requestHandlers.favicon;
 handle["/jquery-1.7.2.min.js"] = requestHandlers.jquery;
