b42ef0e19ab0c28122acd6a5b4a1f45559328759
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-05-10 14:11:32 +0000

    [nodejs::who_desktop] Messaging Service : Join Message (WTS), HTTP Chatting

--- trunk/incubator/nodejs/who_desktop/requestHandlers.js
+++ trunk/incubator/nodejs/who_desktop/requestHandlers.js
@@ -1,14 +1,14 @@
-var fs = require("fs"),
-    jade = require('jade'),
-	url = require('url'),
-	wts = require('./wts');
+var fs = require("fs"),
+    jade = require('jade'),
+	url = require('url'),
+	wts = require('./wts'),
+	qs = require('querystring');
 
 var jadefile = fs.readFileSync(__dirname + '/start.jade');
 var fn = jade.compile(jadefile.toString('utf8'));
 
 function start(response) {
     console.log("Request handler 'start' was called.");
-
     var data = wts.enumerate();
     var json_data = eval('(' + data + ')');
     var html = fn(json_data);
@@ -32,26 +32,49 @@ function favicon(response) {
   });
 }
 
+// this <- message()
+function messageResponder(response, query) {
+    var to = query["to"];
+    var msg = query["msg"];
+    
+    try {
+        var res = wts.message(parseInt(to, 10), msg);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(res);
+        response.end();
+    } catch (e) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write('<html><body><h1>Insufficien Parameter</h1>'
+            + '<p>example: /message?to={number}&msg="{message}"</p></body>');
+        response.end();
+    }
+}
+
 function message(response, request) {
-	// refs. http://nodejs.org/docs/v0.6.15/api/url.html
-	var urlObj = url.parse(request.url, true);
-	var q = urlObj.query;
-	var to = q["to"];
-	var msg = q["msg"];
-
-	console.log("Request handler 'message' was called.");
-
-	try {
-	    var res = wts.message(parseInt(to, 10), msg);
-	    response.writeHead(200, { "Content-Type": "text/html" });
-	    response.write(res);
-	    response.end();
-	} catch (e) {
-	    response.writeHead(200, { "Content-Type": "text/html" });
-	    response.write('<html><body><h1>Insufficien Parameter</h1>'
-	        + '<p>example: /message?to={number}&msg="{message}"</p></body>');
-	    response.end();
-	}
+    console.log("Request handler 'message' was called.");
+
+    if ('POST' == request.method) {
+        var postData = "";
+        request.addListener("data", function(chunk) {
+            postData += chunk;
+        });
+        request.addListener("end", function() {
+            var q = qs.parse(postData);
+            messageResponder(response, q);
+        });
+        return;
+
+    } else if ('GET' == request.method) {
+        // refs. http://nodejs.org/docs/v0.6.15/api/url.html
+        var urlObj = url.parse(request.url, true);
+        var q = urlObj.query;
+        messageResponder(response, q);
+    } else {
+        response.writeHead(403, { "Content-Type": "text/html" });
+        response.write("Allow Only GET & POST");
+        response.end();
+        return;
+    }
 }
 
 exports.start = start;
