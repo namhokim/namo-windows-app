f54cfbc71740f98460abacf9fa2e91c695984add
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2012-05-11 09:16:41 +0000

    [nodejs::8282::who_desktop] express framework

---
+++ category/nodejs/8282/who_desktop/routes/index.js
@@ -0,0 +1,30 @@
+var wts = require('./wts');
+
+/*
+ * GET home page.
+ */
+exports.index = function(req, res){
+  var data = wts.enumerate();
+  var json_data = eval('(' + data + ')');
+  res.render('index', json_data)
+};
+
+/*
+ * POST message page.
+ */
+exports.message = function(req, res){
+    var to = req.param('to');
+    var msg = req.param('msg');
+    
+    try {
+        var r = wts.message(parseInt(to, 10), msg);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(r);
+        res.end();
+    } catch (e) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('<html><body><h1>Insufficien Parameter</h1>'
+            + '<p>example: /message?to={number}&msg="{message}"</p></body>');
+        res.end();
+    }
+};
---
+++ category/nodejs/8282/who_desktop/sock_server.js
@@ -0,0 +1,26 @@
+var socketio = require("socket.io");
+
+exports.start = function(server) {
+    var io = socketio.listen(server);
+    var clients = 0;    // http://stackoverflow.com/questions/7352164/update-all-clients-using-socket-io
+    io.set('log level', 2); // https://github.com/LearnBoost/Socket.IO/wiki/Configuring-Socket.IO
+    io.set('transports', [
+	    'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
+      ]);
+    io.sockets.on('connection', function(socket) {
+        ++clients;
+        socket.broadcast.emit('users_count', { number: clients });
+
+        // message event
+        socket.on('message', function(data) {
+            // emit message
+            io.sockets.emit('message', data);
+        });
+        socket.on('disconnect', function() {
+            --clients;
+            socket.broadcast.emit('users_count', { number: clients });  // except for self
+        });
+
+        io.sockets.sockets[socket.id].emit('users_count', { number: clients, id: socket.id }); // to self
+    });
+};
