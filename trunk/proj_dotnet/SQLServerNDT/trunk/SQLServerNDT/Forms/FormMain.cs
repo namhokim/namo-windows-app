ea8fddb6025594790908e04bf6ad31cf0d505a28
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-11 11:41:18 +0000

    DB - Query but It's not working.

--- trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
+++ trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,47 @@ namespace SQLServerNDT.Forms
 
         private void buttonConnectLocal_Click(object sender, EventArgs e)
         {
+            CloseDatabase();
             FormConnection conn = new FormConnection();
-            _connection = conn.Connection;
+            if (conn.ShowDialog() == DialogResult.OK)
+            {
+                _connection = conn.Connection;
+                this.buttonConnInfo.Enabled = true;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            CloseDatabase();
+            base.OnClosed(e);
+        }
+
+        void CloseDatabase()
+        {
+            if (_connection != null && _connection.State==ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+
+        private void buttonConnInfo_Click(object sender, EventArgs e)
+        {
+            string query = @"SELECT S.SPID, S.LOGINAME, S.LOGIN_TIME, S.LAST_BATCH, C.CLIENT_NET_ADDRESS FROM sys.sysprocesses S, sys.dm_exec_connections C WHERE S.spid = C.SESSION_ID";
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(query, _connection);
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(da);
+                DataTable dt = new DataTable();
+                da.Fill(new DataTable());
+                this.dataGridView.DataSource = dt;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(this, ex.Message, "쿼리 실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //this._connection.
+            //;this.dataGridView.DataSource = 
         }
     }
 }
