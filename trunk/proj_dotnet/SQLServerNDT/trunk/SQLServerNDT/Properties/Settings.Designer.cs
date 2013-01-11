c0992afadeeb9fdd2e67c1e8319c89bb23761bff
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-11 10:24:23 +0000

    접속까지 완료, TODO: 연결실패시 처리, 취소

--- trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Properties/Settings.Designer.cs
+++ trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Properties/Settings.Designer.cs
@@ -27,7 +27,7 @@ namespace SQLServerNDT.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>localhost</string>\r\n</ArrayOfString>")]
+            "tring>(local)</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection ServerNames {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["ServerNames"]));
