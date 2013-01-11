e8dd53ec05206d1830940831ce67e10923531ce6
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-11 04:22:14 +0000

    SQL Server Namo Diagnose Tool

---
+++ proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQLServerNDT.Forms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnectLocal_Click(object sender, EventArgs e)
+        {
+            //ConnectionDialog dialog = new ConnectionDialog();
+            //dialog.ShowDialog();
+            FormConnection conn = new FormConnection();
+            conn.ShowDialog();
+        }
+    }
+}
