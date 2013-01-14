238bde47f9acc613ab8f49d9f52344a52222d87c
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-14 05:25:38 +0000

    Query - 연결정보보기, CPU병목보기, CPU를 가장 많이 소비하는 대상보기

--- proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
+++ proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
@@ -122,5 +122,40 @@ namespace SQLServerNDT.Forms
             }
         }
 
+        private void dataGridView_DataError(object sender, DataGridViewDataErrorEventArgs anError)
+        {
+            dataGridView.Rows[anError.RowIndex].ErrorText = anError.Exception.Message;
+            anError.Cancel = true;
+
+            // http://msdn.microsoft.com/en-us/library/system.windows.forms.datagridview.dataerror.aspx
+            //MessageBox.Show("Error happened " + anError.Context.ToString());
+
+            if (anError.Context == DataGridViewDataErrorContexts.Commit)
+            {
+                MessageBox.Show("Commit error");
+            }
+            if (anError.Context == DataGridViewDataErrorContexts.CurrentCellChange)
+            {
+                MessageBox.Show("Cell change");
+            }
+            if (anError.Context == DataGridViewDataErrorContexts.Parsing)
+            {
+                MessageBox.Show("parsing error");
+            }
+            if (anError.Context == DataGridViewDataErrorContexts.LeaveControl)
+            {
+                MessageBox.Show("leave control error");
+            }
+
+            if ((anError.Exception) is ConstraintException)
+            {
+                DataGridView view = (DataGridView)sender;
+                view.Rows[anError.RowIndex].ErrorText = "an error";
+                view.Rows[anError.RowIndex].Cells[anError.ColumnIndex].ErrorText = "an error";
+
+                anError.ThrowException = false;
+            }
+        }
+
     }
 }
