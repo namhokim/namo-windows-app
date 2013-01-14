2c2f2089791160cb1cf5d735f3b0493a3442f697
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-14 06:06:35 +0000

    수행동작(표시/쿼리) - 리소스화 (쿼리 재컴파일 횟수+)

--- proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/QueryLists.cs
+++ proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/QueryLists.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SQLServerNDT.Properties;
 
 namespace SQLServerNDT
 {
@@ -9,9 +10,11 @@ namespace SQLServerNDT
     {
         public static void SetItems(System.Collections.IList list)
         {
-            list.Add(new QueryObject { Display = "연결정보보기", Query = @"SELECT S.SPID, RTRIM(S.LOGINAME) as LOGINAME, S.LOGIN_TIME, S.LAST_BATCH, C.CLIENT_NET_ADDRESS FROM sys.sysprocesses S, sys.dm_exec_connections C WHERE S.spid = C.SESSION_ID;" });
-            list.Add(new QueryObject { Display = "CPU병목보기 - runnable_tasks_count가 높은 값일 수록 CPU병목", Query = @"select scheduler_id, current_tasks_count, runnable_tasks_count from sys.dm_os_schedulers where scheduler_id < 255;" });
-            list.Add(new QueryObject { Display = "CPU를 가장 많이 소비하는 대상보기 - 캐시된 일괄처리, 프로시저", Query = @"select top 50 SUM(qs.total_worker_time) as total_cpu_time, SUM(qs.execution_count) as total_execution_count, COUNT(*) as number_of_statements, CONVERT(VARCHAR(64), qs.plan_handle, 1) AS plan_handle from sys.dm_exec_query_stats qs group by qs.plan_handle order by SUM(qs.total_worker_time) desc;" });
+
+            list.Add(new QueryObject { Display = Resources.Q01_D, Query = Resources.Q01_Q });
+            list.Add(new QueryObject { Display = Resources.Q02_D, Query = Resources.Q02_Q });
+            list.Add(new QueryObject { Display = Resources.Q03_D, Query = Resources.Q03_Q });
+            list.Add(new QueryObject { Display = Resources.Q04_D, Query = Resources.Q04_Q });
             //list.Add(new QueryObject { Display = "표시", Query = @"쿼리" });
         }
     }
