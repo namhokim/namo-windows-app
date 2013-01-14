3cba7e4a47e2cce86c51ccd35ca09a8d9affdaad
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-14 04:27:23 +0000

    UI - for multiple query template

--- proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
+++ proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.cs
@@ -12,6 +12,11 @@ namespace SQLServerNDT.Forms
         public FormMain()
         {
             InitializeComponent();
+            InitializeQueryCombo();
+        }
+
+        private void InitializeQueryCombo(){
+            QueryLists.SetItems(comboBoxQueries.Items);
         }
 
         private void buttonConnectLocal_Click(object sender, EventArgs e)
@@ -32,60 +37,89 @@ namespace SQLServerNDT.Forms
             }
         }
 
-        void ClearDataGridView()
+        private void ClearDataGridView()
         {
             // http://stackoverflow.com/questions/7430933/datagridview-remove-all-columns
             this.dataGridView.Columns.Clear();
             this.dataGridView.DataSource = null;
         }
 
-        private void buttonConnInfo_Click(object sender, EventArgs e)
+        private void RefreshUI_DB_OK(bool bConnected)
         {
-            string query = @"SELECT S.SPID, RTRIM(S.LOGINAME) as LOGINAME, S.LOGIN_TIME, S.LAST_BATCH, C.CLIENT_NET_ADDRESS FROM sys.sysprocesses S, sys.dm_exec_connections C WHERE S.spid = C.SESSION_ID";
+            if (bConnected)
+            {
+                this.buttonConnect.Text = "끊기(&D)";
+                this.comboBoxQueries.Enabled = true;
+            }
+            else
+            {
+                this.buttonConnect.Text = "연결(&C)";
+                this.comboBoxQueries.Enabled = false;
+            }
+        }
 
+        private void comboBoxQueries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            const int NO_SELECTED = -1;
+
+            if (comboBoxQueries.SelectedIndex == NO_SELECTED)
+            {
+                textBoxQuery.Text = "";
+            }
+            else
+            {
+                var qo = comboBoxQueries.SelectedItem as QueryObject;
+                textBoxQuery.Text = qo.Query;
+            }
+        }
+
+        private void textBoxQuery_TextChanged(object sender, EventArgs e)
+        {
+            buttonExecuteQuery.Enabled = (comboBoxQueries.Enabled && textBoxQuery.TextLength > 0);
+        }
+
+        private void buttonExecuteQuery_Click(object sender, EventArgs e)
+        {
+            ClearDataGridView();
             try
             {
-                ClearDataGridView();
-                using (SqlConnection _connection = new SqlConnection(_connectionString))
-                {
-                    _connection.Open();
-
-                    // http://social.msdn.microsoft.com/Forums/en-US/csharpgeneral/thread/e6698cad-15f7-41e7-82c3-cc71c17f30e2/
-                    SqlDataAdapter da = new SqlDataAdapter(query, _connection);
-                    SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(da);
-
-                    // Populate a new data table and bind it to the BindingSource.
-                    DataTable dt = new DataTable
-                    {
-                        Locale = System.Globalization.CultureInfo.InvariantCulture
-                    };
-                    da.Fill(dt);
-                    this.dataGridView.DataSource = dt;
-
-                    // Resize the DataGridView columns to fit the newly loaded content.
-                    this.dataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                }
+                ExecuteQuery(_connectionString, textBoxQuery.Text, this.dataGridView);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(this, ex.Message, "쿼리실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
 
-        void RefreshUI_DB_OK(bool bConnected)
+        private void ExecuteQuery(string connectionString, string query, DataGridView dataGridView)
         {
-            if (bConnected)
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                this.buttonConnInfo.Enabled = true;
-                this.buttonConnect.Text = "끊기(&D)";
-            }
-            else
-            {
-                this.buttonConnInfo.Enabled = false;
-                this.buttonConnect.Text = "연결(&C)";
-            }
+                conn.Open();
 
+                // http://social.msdn.microsoft.com/Forums/en-US/csharpgeneral/thread/e6698cad-15f7-41e7-82c3-cc71c17f30e2/
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(da);
+
+                // Populate a new data table and bind it to the BindingSource.
+                DataTable dt = new DataTable
+                {
+                    Locale = System.Globalization.CultureInfo.InvariantCulture
+                };
+                da.Fill(dt);
+                dataGridView.DataSource = dt;
+
+                // Resize the DataGridView columns to fit the newly loaded content.
+                dataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+        }
+
+        private void textBoxQuery_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                textBoxQuery.SelectAll();
+            }
         }
 
     }
