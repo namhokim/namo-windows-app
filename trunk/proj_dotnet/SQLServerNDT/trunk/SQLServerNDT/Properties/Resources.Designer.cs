9fe607a7641127f75e685d69b1d99c53de27427f
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-14 06:06:35 +0000

    수행동작(표시/쿼리) - 리소스화 (쿼리 재컴파일 횟수+)

--- trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Properties/Resources.Designer.cs
+++ trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Properties/Resources.Designer.cs
@@ -59,5 +59,88 @@ namespace SQLServerNDT.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   연결정보보기과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q01_D {
+            get {
+                return ResourceManager.GetString("Q01_D", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   SELECT S.SPID, RTRIM(S.LOGINAME) as LOGINAME, S.LOGIN_TIME, S.LAST_BATCH, C.CLIENT_NET_ADDRESS
+        /// FROM sys.sysprocesses S, sys.dm_exec_connections C WHERE S.spid = C.SESSION_ID;과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q01_Q {
+            get {
+                return ResourceManager.GetString("Q01_Q", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   CPU병목보기 - runnable_tasks_count가 높은 값일 수록 CPU병목과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q02_D {
+            get {
+                return ResourceManager.GetString("Q02_D", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   select scheduler_id, current_tasks_count, runnable_tasks_count
+        /// from sys.dm_os_schedulers where scheduler_id &lt; 255;과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q02_Q {
+            get {
+                return ResourceManager.GetString("Q02_Q", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   CPU를 가장 많이 소비하는 대상보기 - 캐시된 일괄처리, 프로시저과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q03_D {
+            get {
+                return ResourceManager.GetString("Q03_D", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   select top 50
+        /// SUM(qs.total_worker_time) as total_cpu_time, SUM(qs.execution_count) as total_execution_count, COUNT(*) as number_of_statements, CONVERT(VARCHAR(64), qs.plan_handle, 1) AS plan_handle
+        /// from sys.dm_exec_query_stats qs
+        /// group by qs.plan_handle
+        /// order by SUM(qs.total_worker_time) desc;과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q03_Q {
+            get {
+                return ResourceManager.GetString("Q03_Q", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   쿼리 재컴파일 횟수(상위 25건의 저장 프로시저)과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q04_D {
+            get {
+                return ResourceManager.GetString("Q04_D", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   select top 25
+        /// sql_text.text, plan_generation_num, execution_count, dbid, objectid
+        /// from sys.dm_exec_query_stats a
+        /// cross apply sys.dm_exec_sql_text(sql_handle) as sql_text
+        /// where plan_generation_num &gt; 1
+        /// order by plan_generation_num desc;과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        internal static string Q04_Q {
+            get {
+                return ResourceManager.GetString("Q04_Q", resourceCulture);
+            }
+        }
     }
 }
