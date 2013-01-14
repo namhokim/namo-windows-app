510e4466f5717fdfcc5cb0fb78574c08618d5c3e
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-01-14 07:00:27 +0000

    UI - save and restore windows position, location, maximum status

--- trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.Designer.cs
+++ trunk/proj_dotnet/SQLServerNDT/trunk/SQLServerNDT/Forms/FormMain.Designer.cs
@@ -34,7 +34,12 @@
             this.textBoxQuery = new System.Windows.Forms.TextBox();
             this.comboBoxQueries = new System.Windows.Forms.ComboBox();
             this.buttonExecuteQuery = new System.Windows.Forms.Button();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonConnect
@@ -56,24 +61,25 @@
             this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.GridColor = System.Drawing.SystemColors.ControlLight;
-            this.dataGridView.Location = new System.Drawing.Point(13, 139);
+            this.dataGridView.Location = new System.Drawing.Point(0, 3);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
             this.dataGridView.RowTemplate.Height = 23;
-            this.dataGridView.Size = new System.Drawing.Size(679, 184);
+            this.dataGridView.Size = new System.Drawing.Size(701, 205);
             this.dataGridView.TabIndex = 4;
             this.dataGridView.TabStop = false;
             this.dataGridView.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView_DataError);
             // 
             // textBoxQuery
             // 
-            this.textBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.textBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxQuery.Location = new System.Drawing.Point(12, 42);
+            this.textBoxQuery.Location = new System.Drawing.Point(3, 3);
             this.textBoxQuery.Multiline = true;
             this.textBoxQuery.Name = "textBoxQuery";
             this.textBoxQuery.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxQuery.Size = new System.Drawing.Size(680, 76);
+            this.textBoxQuery.Size = new System.Drawing.Size(698, 65);
             this.textBoxQuery.TabIndex = 3;
             this.textBoxQuery.TextChanged += new System.EventHandler(this.textBoxQuery_TextChanged);
             this.textBoxQuery.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxQuery_KeyDown);
@@ -102,21 +108,57 @@
             this.buttonExecuteQuery.UseVisualStyleBackColor = true;
             this.buttonExecuteQuery.Click += new System.EventHandler(this.buttonExecuteQuery_Click);
             // 
+            // splitContainer
+            // 
+            this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer.Location = new System.Drawing.Point(0, 42);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.textBoxQuery);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.dataGridView);
+            this.splitContainer.Size = new System.Drawing.Size(704, 281);
+            this.splitContainer.SplitterDistance = 69;
+            this.splitContainer.TabIndex = 5;
+            this.splitContainer.TabStop = false;
+            this.splitContainer.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer_SplitterMoved);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 313);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(704, 22);
+            this.statusStrip.TabIndex = 6;
+            this.statusStrip.Text = "statusStrip1";
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 335);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.splitContainer);
             this.Controls.Add(this.buttonExecuteQuery);
             this.Controls.Add(this.comboBoxQueries);
-            this.Controls.Add(this.textBoxQuery);
-            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.buttonConnect);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(500, 300);
             this.Name = "FormMain";
             this.Text = "SQL Server 나모진단도구";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,6 +171,8 @@
         private System.Windows.Forms.TextBox textBoxQuery;
         private System.Windows.Forms.ComboBox comboBoxQueries;
         private System.Windows.Forms.Button buttonExecuteQuery;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.StatusStrip statusStrip;
     }
 }
 
