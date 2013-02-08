9a0400aaf50487498ee171184a95bcd728188c2e
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-02-08 04:38:08 +0000

    til call ExtensionQuery

--- trunk/category/network/snmp/SnmpExt/SnmpExtension.cpp
+++ trunk/category/network/snmp/SnmpExt/SnmpExtension.cpp
@@ -2,77 +2,158 @@
 #include "SnmpExtension.h"
 #include <Snmp.h>
 
+#pragma comment(lib, "Snmpapi.lib")
+
 // http://msdn.microsoft.com/en-us/library/windows/desktop/aa378018(v=vs.85).aspx
 
 //#pragma comment(lib, "Snmpapi.lib")	// for _SnmpUtilVarBindFree@4, _SnmpUtilOidNCmp@12, _SnmpUtilOidCpy@8 
 //#pragma comment(lib, "inetmib1.lib")
 
-typedef DWORD (WINAPI * pSSGU) ();	// SnmpSvcGetUptime
+// SnmpSvcGetUptime
+// http://msdn.microsoft.com/en-us/library/windows/desktop/aa378288(v=vs.85).aspx
+typedef DWORD (WINAPI * pSSGU) (void);	
 
+// SnmpExtensionInit
+// http://msdn.microsoft.com/en-us/library/windows/desktop/aa378018(v=vs.85).aspx
 typedef BOOL(WINAPI * pSEI) (
-	IN DWORD dwTimeZeroReference,
-	OUT HANDLE * hPollForTrapEvent,
-	OUT AsnObjectIdentifier * supportedView);
+	_In_  DWORD dwUptimeReference,
+	_Out_ HANDLE * phSubagentTrapEvent,
+	_Out_ AsnObjectIdentifier * pFirstSupportedRegion);
 
+// SnmpExtensionQuery
+// http://msdn.microsoft.com/en-us/library/windows/desktop/aa378021(v=vs.85).aspx
 typedef BOOL(WINAPI * pSEQ) (
-	IN BYTE requestType,
-	IN OUT RFC1157VarBindList * variableBindings,
-	OUT AsnInteger * errorStatus,
-	OUT AsnInteger * errorIndex);
-
-typedef void (WINAPI * pSEC) ();	// SnmpExtensionClose
+	_In_     BYTE bPduType,
+	_Inout_  SnmpVarBindList *pVarBindList,
+	_Out_    AsnInteger32 *pErrorStatus,
+	_Out_    AsnInteger32 *pErrorIndex);
 
 namespace Snmp
 {
-	// http://msdn.microsoft.com/en-us/library/windows/desktop/aa378288(v=vs.85).aspx
 	pSSGU SvcGetUptime = NULL;
 	pSEI ExtensionInit = NULL;
 	pSEQ ExtensionQuery = NULL;
-	// http://msdn.microsoft.com/en-us/library/windows/desktop/aa378017(v=vs.85).aspx
-	pSEC ExtensionClose = NULL;
 
 	BOOL InitLibrary();
 }	// namespace snmp
 
 void foo()
 {
+	BOOL bRes;
+
 	DWORD dwTick;
-	HANDLE PollForTrapEvent;
-	AsnObjectIdentifier SupportedView;
+	HANDLE SubagentTrapEvent;
+	AsnObjectIdentifier FirstSupportedRegion;
+
+	// Initialize the variable list
+	SnmpVarBindList VarBindList;
+	AsnInteger32 ErrorStatus;
+	AsnInteger32 ErrorIndex;
+
+	const int LenVarBind = 2;
+	SnmpVarBind VarBind[LenVarBind];
+
+	VarBindList.list = VarBind;
+	VarBindList.len = 1;
+
+	AsnObjectIdentifier MIB_NULL = {0, 0};
+	VarBind[0].name = MIB_NULL;
+	VarBind[1].name = MIB_NULL;
+
+	UINT OID_sysDescr[] = {1, 3, 6, 1, 2, 1, 1, 1, 0};
+	AsnObjectIdentifier MIB_ifEntryType = 
+	{sizeof(OID_sysDescr) / sizeof(UINT), OID_sysDescr};
+
+	SnmpUtilOidCpy(&VarBind[0].name, &MIB_ifEntryType);
 	
 	// Check
 	if (Snmp::InitLibrary()==FALSE) return;
 
 	// Main
 	dwTick = Snmp::SvcGetUptime();
-	Snmp::ExtensionInit(dwTick, &PollForTrapEvent, &SupportedView);
+	bRes = Snmp::ExtensionInit(dwTick, &SubagentTrapEvent, &FirstSupportedRegion);
+	if (bRes==FALSE) return;
 
-	Snmp::ExtensionClose();
+	Snmp::ExtensionQuery(SNMP_PDU_GET, &VarBindList, &ErrorStatus, &ErrorIndex);
+
+	printf("Result: %d\n", ErrorStatus);
 
 	return;
 }
 
 namespace Snmp
 {
-	BOOL InitLibrary()
+	LPCTSTR szSnmpapi_dll = TEXT("Snmpapi.dll");
+	LPCTSTR szInetmib1_dll = TEXT("inetmib1.dll");
+	// Windows 2000 - C:\WINNT\system32\inetmib1.dll
+	// Windows 7 - C:\Windows\SysWOW64\inetmib1.dll
+	// SnmpExtensionInit
+	// SnmpExtensionInitEx
+	// SnmpExtensionQuery
+	// SnmpExtensionTrap
+
+	BOOL ResetInitLibrary()
+	{
+		pSSGU SvcGetUptime = NULL;
+		pSEI ExtensionInit = NULL;
+		pSEQ ExtensionQuery = NULL;
+
+		return FALSE;
+	}
+
+	BOOL InitSnmpapi()
 	{
 		// Prevent re-initialize
 		if (Snmp::SvcGetUptime!=NULL) return TRUE;
 
 		// DLL load
-		HMODULE m_hInst = GetModuleHandle(TEXT("inetmib1.dll"));
-		if (m_hInst < (HINSTANCE) HINSTANCE_ERROR)
+		HMODULE hDll = GetModuleHandle(szSnmpapi_dll);
+		if (hDll == NULL)
 		{
-			m_hInst = NULL;
-			return FALSE;
+			hDll = LoadLibrary(szSnmpapi_dll);
+			if (hDll == NULL)
+			{
+				return FALSE;
+			}
 		}
 
 		// Initialize the Dll functions pointers
-		SvcGetUptime = (pSSGU)GetProcAddress(m_hInst, "SnmpSvcGetUptime");
-		ExtensionInit = (pSEI)GetProcAddress(m_hInst, "SnmpExtensionInit");
-		ExtensionQuery = (pSEQ)GetProcAddress(m_hInst, "SnmpExtensionQuery");
-		ExtensionClose = (pSEC)GetProcAddress(m_hInst, "SnmpExtensionClose");
+		SvcGetUptime = (pSSGU)GetProcAddress(hDll, "SnmpSvcGetUptime");
+		if (ExtensionInit==NULL) return ResetInitLibrary();
 
 		return TRUE;
 	}
+
+	BOOL InitInetmib1()
+	{
+		// Prevent re-initialize
+		if (ExtensionInit!=NULL) return TRUE;
+
+		// DLL load
+		HMODULE hDll = GetModuleHandle(szInetmib1_dll);
+		if (hDll == NULL)
+		{
+			hDll = LoadLibrary(szInetmib1_dll);
+			if (hDll == NULL)
+			{
+				return FALSE;
+			}
+		}
+
+		// Initialize the Dll functions pointers
+		ExtensionInit = (pSEI)GetProcAddress(hDll, "SnmpExtensionInit");
+		if (ExtensionInit==NULL) return ResetInitLibrary();
+		ExtensionQuery = (pSEQ)GetProcAddress(hDll, "SnmpExtensionQuery");
+		if (ExtensionQuery==NULL) return ResetInitLibrary();
+
+		return TRUE;
+	}
+
+	// this -> InitInetmib1() | InitSnmpapi()
+	BOOL InitLibrary()
+	{
+		return (InitInetmib1() && InitSnmpapi());
+	}
+
+	// Sample Extension-Agent DLL - http://msdn.microsoft.com/en-us/library/aa913887.aspx
 }	// namespace snmp
