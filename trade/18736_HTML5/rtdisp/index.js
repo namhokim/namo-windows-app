7750f072a058ec860220c7d1e24fb0a7a19328d5
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-02-20 11:39:25 +0000

    add whole UI components

--- trade/18736_HTML5/rtdisp/index.js
+++ trade/18736_HTML5/rtdisp/index.js
@@ -4,6 +4,9 @@ var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 handle["/"] = requestHandlers.mobile;
+handle["/connect"] = requestHandlers.connect;
+handle["/antechamber"] = requestHandlers.antechamber;
+handle["/edit"] = requestHandlers.edit;
 handle["/draw"] = requestHandlers.draw;
 handle["/favicon.ico"] = requestHandlers.favicon;
 
--- trade/18736_HTML5/rtdisp/requestHandlers.js
+++ trade/18736_HTML5/rtdisp/requestHandlers.js
@@ -4,6 +4,7 @@ var querystring = require("querystring"),
 var DEFAULT_ENCODING = 'utf8';
 var RAW_ENCODING = 'binary';
 
+// #1. 모바일 초기화면
 function mobile(response, postData) {
   fs.readFile(__dirname + '/index.html', DEFAULT_ENCODING,
     function(error, file) {
@@ -19,6 +20,55 @@ function mobile(response, postData) {
     });
 }
 
+// #2. 모바일 접속중 화면
+function connect(response, postData) {
+  fs.readFile(__dirname + '/connecting.html', DEFAULT_ENCODING,
+    function(error, file) {
+      if(error) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(error + "\n");
+        response.end();
+      } else {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(file, DEFAULT_ENCODING);
+        response.end();
+      }
+    });
+}
+
+// #3. 모바일 작업시작 화면
+function antechamber(response, postData) {
+  fs.readFile(__dirname + '/antechamber.html', DEFAULT_ENCODING,
+    function(error, file) {
+      if(error) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(error + "\n");
+        response.end();
+      } else {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(file, DEFAULT_ENCODING);
+        response.end();
+      }
+    });
+}
+
+// #4. 모바일 편집 화면
+function edit(response, postData) {
+  fs.readFile(__dirname + '/edit.html', DEFAULT_ENCODING,
+    function(error, file) {
+      if(error) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(error + "\n");
+        response.end();
+      } else {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(file, DEFAULT_ENCODING);
+        response.end();
+      }
+    });
+}
+
+// #5. 데스트탑 디스플레이 화면
 function draw(response, postData) {
   fs.readFile(__dirname + '/draw.html', DEFAULT_ENCODING,
     function(error, file) {
@@ -51,5 +101,8 @@ function favicon(response, postData) {
 
 
 exports.mobile = mobile;
+exports.connect = connect;
+exports.antechamber = antechamber;
+exports.edit = edit;
 exports.draw = draw;
 exports.favicon = favicon;
