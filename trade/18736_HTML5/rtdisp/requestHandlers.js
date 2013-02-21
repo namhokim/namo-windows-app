f14e35c07344cc5ef437a8fdb74bfc9620c9d410
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-02-21 11:13:45 +0000

    refactoring : static resource

--- trade/18736_HTML5/rtdisp/requestHandlers.js
+++ trade/18736_HTML5/rtdisp/requestHandlers.js
@@ -4,101 +4,10 @@ var querystring = require("querystring"),
 var DEFAULT_ENCODING = 'utf8';
 var RAW_ENCODING = 'binary';
 
-// #1. 모바일 초기화면
-function mobile(response, postData) {
-  fs.readFile(__dirname + '/index.html', DEFAULT_ENCODING,
-    function(error, file) {
-      if(error) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(error + "\n");
-        response.end();
-      } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(file, DEFAULT_ENCODING);
-        response.end();
-      }
-    });
-}
+//////////////////////////////////////////////////////////////////////////
 
-// #2. 모바일 접속중 화면
-function connect(response, postData) {
-  fs.readFile(__dirname + '/connecting.html', DEFAULT_ENCODING,
-    function(error, file) {
-      if(error) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(error + "\n");
-        response.end();
-      } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(file, DEFAULT_ENCODING);
-        response.end();
-      }
-    });
-}
-
-// #3. 모바일 작업시작 화면
-function antechamber(response, postData) {
-  fs.readFile(__dirname + '/antechamber.html', DEFAULT_ENCODING,
-    function(error, file) {
-      if(error) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(error + "\n");
-        response.end();
-      } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(file, DEFAULT_ENCODING);
-        response.end();
-      }
-    });
-}
-
-// #4. 모바일 편집 화면
-function edit(response, postData) {
-  fs.readFile(__dirname + '/edit.html', DEFAULT_ENCODING,
-    function(error, file) {
-      if(error) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(error + "\n");
-        response.end();
-      } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(file, DEFAULT_ENCODING);
-        response.end();
-      }
-    });
-}
-
-// #5. 데스트탑 디스플레이 화면
-function draw(response, postData) {
-  fs.readFile(__dirname + '/draw.html', DEFAULT_ENCODING,
-    function(error, file) {
-      if(error) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(error + "\n");
-        response.end();
-      } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(file, DEFAULT_ENCODING);
-        response.end();
-      }
-    });
-}
-
-function favicon(response, postData) {
-  fs.readFile(__dirname + '/favicon.ico', RAW_ENCODING,
-    function(error, file) {
-      if(error) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(error + "\n");
-        response.end();
-      } else {
-        response.writeHead(200, {"Content-Type": "image/vnd.microsoft.icon"});
-        response.write(file, RAW_ENCODING);
-        response.end();
-      }
-    });
-}
-
+// ex. getLastPart('/static/favicon.ico', '.')  => ico
+// ex. getLastPart('/static/index.html', '.')  => html
 function getLastPart(targetString, delimeter)
 {
   if (delimeter.length > targetString.length) {
@@ -108,27 +17,29 @@ function getLastPart(targetString, delimeter)
     var p = targetString.lastIndexOf(delimeter);
     if (p == -1) return '';
     var i = p + delimeter.length;
-      document.write( i );
     var r = targetString.substring(i);  
     return r;
   }
 }
 
-function static_handler(response, resource, pathname) {
+// 정적 파일 핸들러
+function static_handler(response, request, pathname) {
   console.log("static_handler for " + pathname);
 
   // MIME판단
-  var enc, type;
-  switch (getLastPart)
+  var enc = DEFAULT_ENCODING
+    , type = 'application/octet-stream';
+  switch (getLastPart(pathname, '.'))
   {
   case 'ico':
     enc = RAW_ENCODING;
     type = "image/vnd.microsoft.icon";
     break;
   case 'html':
-  default:
-    enc = DEFAULT_ENCODING;
     type = "text/html";
+    break;
+  case 'css':
+    type = "text/css";
     break;  
   }
 
@@ -145,6 +56,38 @@ function static_handler(response, resource, pathname) {
         response.end();
       }
     });
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+// #1. 모바일 초기화면
+function mobile(response, request) {
+  static_handler(response, request, '/static/index.html');
+}
+
+// #2. 모바일 접속중 화면
+function connect(response, request) {
+  static_handler(response, request, '/static/connecting.html');
+}
+
+// #3. 모바일 작업시작 화면
+function antechamber(response, request) {
+  static_handler(response, request, '/static/antechamber.html');
+}
+
+// #4. 모바일 편집 화면
+function edit(response, request) {
+  static_handler(response, request, '/static/edit.html');
+}
+
+// #5. 데스트탑 디스플레이 화면
+function draw(response, request) {
+  static_handler(response, request, '/static/draw.html');
+}
+
+// favicon.ico 요청 처리
+function favicon(response, request) {
+  static_handler(response, request, '/static/favicon.ico');
 }
 
 
