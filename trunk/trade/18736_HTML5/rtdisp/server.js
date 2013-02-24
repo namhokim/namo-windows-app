dda4cb90391ee1dffc3d793c1b2f869d2d7ecf19
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-02-24 05:12:43 +0000

    code refine

--- trunk/trade/18736_HTML5/rtdisp/server.js
+++ trunk/trade/18736_HTML5/rtdisp/server.js
@@ -1,18 +1,28 @@
-var http = require("http")
-  , url = require("url");
-
 function start(route, handle, port) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-    route(handle, pathname, response, request);
-  }
 
-  if (typeof port === "undefined") port = 80;
+	// handler
+	function onRequest(request, response) {
+		var url = require("url");
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response, request);
+	}
 
-  var app = http.createServer(onRequest).listen(port);
-  var io = require('socket.io').listen(app);
-  console.log("Server has started on " + port + ".");
+	// start
+	if (typeof port === "undefined") port = 80;
+
+	var app = require('http').createServer(onRequest)
+		, io = require('socket.io').listen(app);
+
+	app.listen(port);
+
+	io.sockets.on('connection', function(socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+			console.log(data);
+		});
+	});
+	console.log("Server has started on " + port + ".");
 }
 
 exports.start = start;
