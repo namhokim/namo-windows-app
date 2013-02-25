b46cdfa7218ec0b80cb4f09e166912deb15e79bd
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-02-25 11:20:01 +0000

    comment

--- trunk/trade/18736_HTML5/rtdisp/server.js
+++ trunk/trade/18736_HTML5/rtdisp/server.js
@@ -15,13 +15,14 @@ function start(route, handle, port) {
 		, io = require('socket.io').listen(app);
 
 	app.listen(port);
-
+/*
 	io.sockets.on('connection', function(socket) {
 		socket.emit('news', { hello: 'world' });
 		socket.on('my other event', function (data) {
 			console.log(data);
 		});
 	});
+	*/
 	console.log("Server has started on " + port + ".");
 }
 
