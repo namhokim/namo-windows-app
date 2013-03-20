3ceacb049b79f6af9e903e00a31b81192f45a584
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-03-20 13:04:48 +0000

    Accelerator (단축키) 지정 +

--- trunk/trade/18986_sudoku/sudoku/resource.h
+++ trunk/trade/18986_sudoku/sudoku/resource.h
@@ -28,14 +28,13 @@
 #define IDC_BUTTON25                    1019
 #define IDC_BUTTON26                    1020
 #define IDC_BUTTON_QUIT                 1021
-#define ID_ACCELERATOR32771             32771
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_COMMAND_VALUE         32778
 #define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
