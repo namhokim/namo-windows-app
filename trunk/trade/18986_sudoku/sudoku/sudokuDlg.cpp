60b528a0ee568acd86aa61dae1548195a5becca5
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-03-20 15:14:24 +0000

    code refining

--- trunk/trade/18986_sudoku/sudoku/sudokuDlg.cpp
+++ trunk/trade/18986_sudoku/sudoku/sudokuDlg.cpp
@@ -13,9 +13,6 @@
 
 // CsudokuDlg 대화 상자
 
-
-
-
 CsudokuDlg::CsudokuDlg(CWnd* pParent /*=NULL*/)
 	: CDialog(CsudokuDlg::IDD, pParent)
 {
@@ -52,82 +49,18 @@ BEGIN_MESSAGE_MAP(CsudokuDlg, CDialog)
 	ON_BN_CLICKED(IDC_BUTTON_QUIT, &CsudokuDlg::OnBnClickedButtonQuit)
 END_MESSAGE_MAP()
 
-
-// CsudokuDlg 메시지 처리기
-
-BOOL CsudokuDlg::OnInitDialog()
+//////////////////////////////////////////////////////////////////////////
+// private:
+void CsudokuDlg::ClearButtonValues()
 {
-	CDialog::OnInitDialog();
-
-	// 이 대화 상자의 아이콘을 설정합니다. 응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
-	//  프레임워크가 이 작업을 자동으로 수행합니다.
-	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
-	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
-
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
-	m_hAccelTable = ::LoadAccelerators(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_ACCELERATOR1));
-
-	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
-}
-
-// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
-//  아래 코드가 필요합니다. 문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
-//  프레임워크에서 이 작업을 자동으로 수행합니다.
-
-void CsudokuDlg::OnPaint()
-{
-	if (IsIconic())
+	CString emptyStr;
+	for(int i=0; i<MAZE_SIZE; i++)
 	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트
-
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
-
-		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
-
-		// 아이콘을 그립니다.
-		dc.DrawIcon(x, y, m_hIcon);
+		for(int j=0; j<MAZE_SIZE; j++)
+		{
+			SetButtonValue(btn[i][j], emptyStr);
+		}
 	}
-	else
-	{
-		CDialog::OnPaint();
-	}
-}
-
-// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
-//  이 함수를 호출합니다.
-HCURSOR CsudokuDlg::OnQueryDragIcon()
-{
-	return static_cast<HCURSOR>(m_hIcon);
-}
-
-
-void CsudokuDlg::OnBnClickedButtonNew()
-{
-	TCHAR szFilter[] = _T("텍스트 문서(*.txt)|*.txt| All Files(*.*)|*.*||");
-	CFileDialog open(TRUE, _T("txt"), _T("*.txt"), OFN_HIDEREADONLY,
-		szFilter);
-	if(open.DoModal() == IDOK)
-	{
-		ClearButtonValues();
-		LoadFromFile(open.m_ofn.lpstrFile);
-	}
-}
-
-BOOL CsudokuDlg::PreTranslateMessage(MSG* pMsg)
-{
-	if (m_hAccelTable!=NULL)
-	{
-		if(TranslateAccelerator(m_hWnd, m_hAccelTable, pMsg))
-			return TRUE;
-	}
-
-	return CDialog::PreTranslateMessage(pMsg);
 }
 
 void CsudokuDlg::LoadFromFile(LPCTSTR file)
@@ -174,29 +107,6 @@ void CsudokuDlg::SetButtonValue(CButton&button, const CString& value)
 	}
 }
 
-void CsudokuDlg::ClearButtonValues()
-{
-	CString emptyStr;
-	for(int i=0; i<MAZE_SIZE; i++)
-	{
-		for(int j=0; j<MAZE_SIZE; j++)
-		{
-			SetButtonValue(btn[i][j], emptyStr);
-		}
-	}
-}
-
-void CsudokuDlg::OnBnClickedButtonSave()
-{
-	TCHAR szFilter[] = _T("텍스트 문서(*.txt)|*.txt| All Files(*.*)|*.*||");
-	CFileDialog open(FALSE, _T("txt"), _T("save.txt"), OFN_OVERWRITEPROMPT,
-		szFilter);
-	if(open.DoModal() == IDOK)
-	{
-		SaveToFile(open.m_ofn.lpstrFile);
-	}
-}
-
 void CsudokuDlg::SaveToFile(LPCTSTR file)
 {
 	std::ofstream os (file, std::ios::out);
@@ -214,8 +124,109 @@ void CsudokuDlg::SaveToFile(LPCTSTR file)
 			os << std::endl;
 		}
 	}
-
 }
+
+//////////////////////////////////////////////////////////////////////////
+
+// CsudokuDlg 메시지 처리기
+
+BOOL CsudokuDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// 이 대화 상자의 아이콘을 설정합니다. 응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	m_hAccelTable = ::LoadAccelerators(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_ACCELERATOR1));
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다. 문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CsudokuDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CsudokuDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// 추가 구현 함수들
+
+// 다이얼로그 기반에 엑셀러레이터(단축키)를 처리하기 위한 함수
+BOOL CsudokuDlg::PreTranslateMessage(MSG* pMsg)
+{
+	if (m_hAccelTable!=NULL)
+	{
+		if(TranslateAccelerator(m_hWnd, m_hAccelTable, pMsg))
+			return TRUE;
+	}
+
+	return CDialog::PreTranslateMessage(pMsg);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// 버튼에 대한 핸들러 함수들
+
+void CsudokuDlg::OnBnClickedButtonNew()
+{
+	TCHAR szFilter[] = _T("텍스트 문서(*.txt)|*.txt| All Files(*.*)|*.*||");
+	CFileDialog open(TRUE, _T("txt"), _T("*.txt"), OFN_HIDEREADONLY,
+		szFilter);
+	if(open.DoModal() == IDOK)
+	{
+		ClearButtonValues();
+		LoadFromFile(open.m_ofn.lpstrFile);
+	}
+}
+
+
+
+void CsudokuDlg::OnBnClickedButtonSave()
+{
+	TCHAR szFilter[] = _T("텍스트 문서(*.txt)|*.txt| All Files(*.*)|*.*||");
+	CFileDialog open(FALSE, _T("txt"), _T("save.txt"), OFN_OVERWRITEPROMPT,
+		szFilter);
+	if(open.DoModal() == IDOK)
+	{
+		SaveToFile(open.m_ofn.lpstrFile);
+	}
+}
+
 void CsudokuDlg::OnBnClickedButtonQuit()
 {
 	EndDialog(0);
