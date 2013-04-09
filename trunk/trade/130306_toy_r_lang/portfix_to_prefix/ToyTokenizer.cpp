0d1afd93858e560ee87ff275c0f26c8340ccd69f
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-09 07:41:22 +0000

    숫자 하나 파싱 (assumeTypeByChar() + )

--- trunk/trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.cpp
+++ trunk/trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.cpp
@@ -1,6 +1,9 @@
 #include "StdAfx.h"
 #include "ToyTokenizer.h"
 
+//////////////////////////////////////////////////////////////////////////
+// public
+
 ToyTokenizer::ToyTokenizer(void)
 :m_current_position(NULL)
 {
@@ -20,8 +23,43 @@ void ToyTokenizer::setProg(const char* prog)
 
 bool ToyTokenizer::getToken(std::string& token, int& type)
 {
-	if(m_current_position==NULL) return false;
+	if(m_current_position==NULL) {
+		type = TOY_R_TOKEN_EOP;
+		return false;
+	}
 
-	const char* ptr = m_current_position;
+	char curr_ch = (*m_current_position);	// 현재 문자열
+	type = assumeTypeByChar(curr_ch);
+	switch(type) {
+		case TOY_R_TOKEN_PARENTHESIS:
+			token.push_back(curr_ch);
+			++m_current_position;	// 다음 위치
+			return true;
+		case TOY_R_TOKEN_DIGIT:
+			token.push_back(curr_ch);
+			++m_current_position;	// 다음 위치
+			return true;
+	}
+
 	return false;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// private
+
+// 괄호인지 판단
+bool is_parenthesis(char ch)
+{
+	return (ch=='(' || ch==')');
+}
+
+int ToyTokenizer::assumeTypeByChar(char ch)
+{
+	if (is_parenthesis(ch)) return TOY_R_TOKEN_PARENTHESIS;
+
+	// ref. http://www.cplusplus.com/reference/cctype/isdigit/
+	if (isdigit(ch)!=0) return TOY_R_TOKEN_DIGIT;
+
+	return TOY_R_TOKEN_EOP;
 }
