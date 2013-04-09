37a0c90c513ff3d9172dd8168e6d2464614d5f98
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-09 07:16:29 +0000

    ToyTokenizer 클래스+

--- trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
+++ trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include "ToyTokenizer.h"
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	string input_1("3");
+	string input_2("abc");
+	string input_3("(1 2 MINUS)");
+
+	ToyTokenizer token;
+	token.setProg(input_1.c_str());
+
+
 	return 0;
 }
 
