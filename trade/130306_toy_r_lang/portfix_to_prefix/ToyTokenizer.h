e4052f6ba31e0998115821cdcd02f2edf9b6da88
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-09 07:27:29 +0000

    member m_current_position+ (and initialize)

--- trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.h
+++ trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.h
@@ -11,5 +11,6 @@ public:
 	bool getToken(std::string& token, int& type);	// 토큰을 가져온다
 
 private:
-	const char* m_prog;
+	const char* m_prog;			// 프로그램을 가리키는 문자열 포인터(NULL 종료)
+	const char* m_current_position;	// 파싱하는 위치(NULL이면 끝, 초기화 필요)
 };
