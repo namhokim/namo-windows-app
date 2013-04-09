bc1b1fad1b09bc56e8b6640fc2f7511f2e97c92a
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-09 15:39:40 +0000

    여러개의 공백 ((2  1  MINUS) 3  MINUS) TESTED

--- trunk/trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
+++ trunk/trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
@@ -8,14 +8,28 @@
 
 using namespace std;
 
+void test(const string& prog);
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	string input_1("3210123");
 	string input_2("abcdeMI added plus");
 	string input_3("(1 2 MINUS)");
+	string input_4("((2  1  MINUS) 3  MINUS)");
+	string input_5("((1 2 MINUS)(3 4 MINUS) IF)");
+	string input_6("((((1 2 MINUS)(3 4 MINUS) MINUS)5 MINUS) 6 MINUS)");
 
+	test(input_4);
+
+	return 0;
+}
+
+void test(const string& prog)
+{
 	ToyTokenizer tokenizer;
-	tokenizer.setProg(input_3.c_str());
+	tokenizer.setProg(prog.c_str());
+
+	cout << prog << endl << endl;
 
 	string token;
 	int type;
@@ -24,7 +38,4 @@ int _tmain(int argc, _TCHAR* argv[])
 		cout << "type : " << type
 			<< " (" << TokenTypeToString(type) << ")" << endl << endl;
 	}
-
-	return 0;
 }
-
