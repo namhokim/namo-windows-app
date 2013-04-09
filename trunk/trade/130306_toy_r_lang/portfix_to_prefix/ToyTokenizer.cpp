091e777821a3af913c51e3280cb28e7e6ade7b06
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-09 07:27:29 +0000

    member m_current_position+ (and initialize)

--- trunk/trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.cpp
+++ trunk/trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.cpp
@@ -2,6 +2,7 @@
 #include "ToyTokenizer.h"
 
 ToyTokenizer::ToyTokenizer(void)
+:m_current_position(NULL)
 {
 }
 
@@ -12,9 +13,15 @@ ToyTokenizer::~ToyTokenizer(void)
 void ToyTokenizer::setProg(const char* prog)
 {
 	m_prog = prog;
+	if (prog==NULL) throw new std::invalid_argument("Not allow, NULL value in prog parameter.");
+
+	m_current_position = m_prog;
 }
 
 bool ToyTokenizer::getToken(std::string& token, int& type)
 {
+	if(m_current_position==NULL) return false;
+
+	const char* ptr = m_current_position;
 	return false;
 }
