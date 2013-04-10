5cb936a94eedf3294ffcb803e3845aaf6991cb2e
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-10 09:06:13 +0000

    refactoring

--- trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.h
+++ trade/130306_toy_r_lang/portfix_to_prefix/ToyTokenizer.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#define TOY_R_TOKEN_NOT_DEFINED	(-1)
-#define TOY_R_TOKEN_EOP			0	// end of program
-#define TOY_R_TOKEN_NUMBER		1	// group
-#define TOY_R_TOKEN_DIGIT		11	// single (internal)
-#define TOY_R_TOKEN_NEGATIVE	12	// single (internal)
-#define TOY_R_TOKEN_STRING		2	// group
-#define TOY_R_TOKEN_ALPHA		21	// single (internal)
-#define TOY_R_TOKEN_RESERVED	3	// 예약어(IF, MINUS)
-#define TOY_R_TOKEN_PARENTHESIS	5	// '(' or ')'
-#define TOY_R_TOKEN_SPACE		7	// 공백 (internal)
+#define TOKEN_NOT_DEFINED	(-1)
+#define TOKEN_EOP			0	// end of program
+#define TOKEN_NUMBER		1	// group
+#define TOKEN_DIGIT			11	// single (internal)
+#define TOKEN_NEGATIVE		12	// single (internal)
+#define TOKEN_STRING		2	// group
+#define TOKEN_ALPHA			21	// single (internal)
+#define TOKEN_RESERVED		3	// 예약어(IF, MINUS)
+#define TOKEN_PARENTHESIS	5	// '(' or ')'
+#define TOKEN_SPACE			7	// 공백 (internal)
 
 #include <string>
 
@@ -23,7 +23,7 @@ public:
 
 private:
 	const char* m_prog;			// 프로그램을 가리키는 문자열 포인터(NULL 종료)
-	const char* m_current_position;	// 파싱하는 위치(NULL이면 끝, 초기화 필요)
+	const char* m_curr_pos;		// 파싱하는 현재위치(NULL이면 끝, 초기화 필요)
 	int assumeTypeByChar(char ch);	// 글자 하나를 가지고 타입을 추측
 };
 
