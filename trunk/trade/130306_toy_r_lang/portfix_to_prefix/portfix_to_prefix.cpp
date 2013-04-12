0f68b2bc948f2537bf0d49ecb79976bd74256200
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-12 09:48:10 +0000

    parse() - 문법체크 함수+

--- trunk/trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
+++ trunk/trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
@@ -9,12 +9,16 @@
 using namespace std;
 
 void test(const string& prog);
+bool parse(const string& prog);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	string input_1("3210123");
-	string input_2("abcdeMI added plus");
-	string input_3("(1 2 MINUS)");
+	string input_2e("abcdeMI added plus");
+	string input_31("1 2 MINUS");
+	string input_32("1 2 IF");
+	string input_33e("1 2 3");
+	string input_34("(1 2 MINUS)");
 	string input_4("((2 1 MINUS)3 MINUS)");
 	string input_5("((1 2 MINUS)(3 4 MINUS) IF)");
 	string input_6("((((1 2 MINUS)(3 4 MINUS) MINUS)5 MINUS) 6 MINUS)");
@@ -23,7 +27,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	string input_9("-3210123-");
 	string input_10("((2 1MINUS)3MINUS)");
 
-	test(input_10);
+	string in(input_10);
+	//test(input_10);
+	cout << "original : " << in << endl << endl;
+	cout << (parse(in) ? "sucess" : "false") << endl;
 
 	return 0;
 }
@@ -42,4 +49,88 @@ void test(const string& prog)
 		cout << "type : " << type
 			<< " (" << TokenTypeToString(type) << ")" << endl << endl;
 	}
+}
+
+enum parsing_state {
+	normal,		// 기본상태
+	terminal,	// 종료상태
+	error,		// 파싱에러
+	need_operator,	// IF, MINUS 필요 상태
+};
+
+bool parse(const string& prog)
+{
+	ToyTokenizer tokenizer;
+	tokenizer.setProg(prog.c_str());
+
+	parsing_state state = normal;
+	string buffer;
+
+	string token;
+	int type;
+	while( state != error && tokenizer.getToken(token, type) ) {
+		cout << token << endl;
+		switch (state) {
+			case normal:	// (, string, number
+				switch(type) {
+					case TOKEN_STRING:
+					case TOKEN_NUMBER:
+						state = terminal;
+						break;
+					case TOKEN_PARENTHESIS:
+						if (token=="(") {
+							string innerProg;
+							if (tokenizer.getInnerProg(innerProg)) {
+								cout << "> " << innerProg << endl;
+								if(parse(innerProg)) {
+									state = terminal;
+									cout << "< true" << endl;
+									break;
+								}
+								
+							}
+						}
+						state = error;
+						break;
+					default:
+						state = error;
+				}
+				break;
+			case terminal:
+				switch(type) {
+					case TOKEN_NUMBER:
+						state = need_operator;
+						break;
+					case TOKEN_PARENTHESIS:
+						if (token=="(") {
+							string innerProg;
+							if (tokenizer.getInnerProg(innerProg)) {
+								cout << "> " << innerProg << endl;
+								if(parse(innerProg)) {
+									state = need_operator;
+									cout << "< true" << endl;
+									break;
+								}
+
+							}
+						}
+						state = error;
+						break;
+					default:
+						state = error;
+				}
+				break;
+			case need_operator:
+				switch(type) {
+					case TOKEN_RESERVED:
+						state = terminal;
+						break;
+					default:
+						state = error;
+						}
+				break;
+		}
+	}
+
+	return (state==terminal);
 }
