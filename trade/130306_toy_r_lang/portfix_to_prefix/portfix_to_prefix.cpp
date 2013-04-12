8fbeffae329621b35fb4904539a11929e3393c5a
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-12 14:35:24 +0000

    종합정리

--- trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
+++ trade/130306_toy_r_lang/portfix_to_prefix/portfix_to_prefix.cpp
@@ -2,15 +2,13 @@
 //
 
 #include "stdafx.h"
-#include <string>
-#include <iostream>
+#include <string>		// for string
+#include <iostream>		// for cout/endl
 #include <algorithm>	// for copy
 #include <iterator>		// for ostream_iterator
 #include "ToyTokenizer.h"
 
 using namespace std;
-
-void test(const string& prog);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -30,24 +28,24 @@ int _tmain(int argc, _TCHAR* argv[])
 	string input_11("abd");
 	string input_12("(abd)");
 
-	string in(input_4);
+	string in(input_4);		// 여기를 변경(위의 문자열 중에 하나로)
 
-	// for prefix
-	//string out;
-	//if(postfix_to_prefix(in.c_str(), out)) {
-	//	cout << "in  : " << in << endl;
-	//	cout << "out : " << out << endl;
-	//}
+	// 1. 전위표기법 변경
+	string out;
+	if(postfix_to_prefix(in.c_str(), out)) {
+		cout << "in(postfix) : " << in << endl;
+		cout << "out(prefix) : " << out << endl << endl;
+	}
 
-	// for intermediate code
-	cout << "in  : " << in << endl << endl;
-	std::vector<std::string> out;
-	if(make_im_code(in.c_str(), out)) {
-		copy(out.begin(), out.end(), ostream_iterator<string>(cout, "\n"));
+	// 2. 중간코드 생성
+	std::vector<std::string> out_imc;
+	if(make_im_code(in.c_str(), out_imc)) {
+		copy(out_imc.begin(), out_imc.end(), ostream_iterator<string>(cout, "\n"));
 
 		cout << endl;
 		int ev;
-		if(evaluation(out, ev)) {
+		// 3. 평가
+		if(evaluation(out_imc, ev)) {
 			cout << "eval : " << ev << endl;
 		}
 		return 0;
@@ -55,35 +53,5 @@ int _tmain(int argc, _TCHAR* argv[])
 		cout << "failed..." << endl;
 	}
 
-	// for parse
-//	int errorPosition;
-//	cout << in << endl;
-//	bool res = parse(in.c_str(), &errorPosition);
-//	if(!res) {
-//		for (int i=1; i<errorPosition; ++i) cout << " ";
-//		cout << "*" << endl;
-////		cout << "position : " << errorPosition << endl;
-//	}
-//	cout << (res ? "success" : "false") << endl;
-
-	// for test
-	//test(in);
-
 	return 0;
-}
-
-void test(const string& prog)
-{
-	ToyTokenizer tokenizer;
-	tokenizer.setProg(prog.c_str());
-
-	cout << prog << endl << endl;
-
-	string token;
-	int type;
-	while( tokenizer.getToken(token, type) ) {
-		cout << token << endl;
-		cout << "type : " << type
-			<< " (" << TokenTypeToString(type) << ")" << endl << endl;
-	}
 }
