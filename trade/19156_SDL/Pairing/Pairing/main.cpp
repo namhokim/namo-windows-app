c6b21983c39c64a4236f9125882f37f0b30c6ce0
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-13 03:18:21 +0000

    기본 SDL 뼈대

---
+++ trade/19156_SDL/Pairing/Pairing/main.cpp
@@ -0,0 +1,11 @@
+#include <SDL.h>
+#include <SDL_image.h>	// for imange
+#include <SDL_ttf.h>	// for text
+
+#pragma comment(lib, "SDL.lib")
+#pragma comment(lib, "SDL_image.lib")
+#pragma comment(lib, "SDLmain.lib")
+
+int main(int argc, char *argv[]){
+	return 0;
+}
