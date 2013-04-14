767120c5a73013fed9f326fa725f0eeba8c2a10f
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-14 13:34:48 +0000

    게임 룰+ (돌리기, 두 개 이상 선택후 커서 이동시 원래 복구)

--- trunk/trade/19156_SDL/Pairing/Pairing/GameLv1.h
+++ trunk/trade/19156_SDL/Pairing/Pairing/GameLv1.h
@@ -12,15 +12,25 @@ public:
 
 	void CursorUP();
 	void CursorDown();
-	void CursorRight();
 	void CursorLeft();
+	void CursorRight();
 	void SpaceDown();
 
 private:
+	typedef enum {
+		first, second, need_reset,
+	} g_status;	// 게임상태
 	SDL_Window* win;
 	int pageID, pageMenu, x, y;
+
+	int openCount;
+	g_status stat;
+	int firstX, firstY, secondX, secondY;
 
 	SDL_Page* GetPage();
 	RECT_INFO* GetSelRect();
 	void updateSelection();
+	bool IsFlipped(int x, int y);
+	void Flip(int x, int y);
+	void ResetFlips();
 };
