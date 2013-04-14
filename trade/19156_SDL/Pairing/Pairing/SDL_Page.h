78b8665fad2934278dd86390aca5f851d05b88de
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-14 01:09:58 +0000

    페이지에 배경색 지정, 키보드로 페이지 전환 구현

--- trade/19156_SDL/Pairing/Pairing/SDL_Page.h
+++ trade/19156_SDL/Pairing/Pairing/SDL_Page.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <SDL.h>
+
 class SDL_Page
 {
 public:
 	SDL_Page(void);
 	~SDL_Page(void);
+
+	void SetBgColor(const Uint8 r, const Uint8 g, const Uint8 b);	// 배경색 지정
+	Uint32 GetBgColor(SDL_Surface *screen);	// 배경색 가져오기(SDL_Window에서 호출)
+
+private:
+	//Uint32 bgColor;
+	// 배경색
+	Uint8 bg_r, bg_g, bg_b;
 };
--- trade/19156_SDL/Pairing/Pairing/SDL_Window.h
+++ trade/19156_SDL/Pairing/Pairing/SDL_Window.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <SDL.h>
+#include <vector>	// for keep pages
 #include "SDL_Page.h"
 
-// Background Colre Pre-define
-#define SDL_BG_COLOR_WHILTE	0x00FFFFFF
+#define PAGE_NOT_SELECTED	(-1)
+#define DEFAULT_PAGE		0		// FIRST PAGE
 
 // implement for SDL functions wrapping
 class SDL_Window
@@ -16,7 +16,10 @@ public:
 	bool Initialize();		// 초기화
 	bool IsInitialized() const { return bInitialized; };// 초기화 여부 확인 함수
 
-	void AddPage(int page_id, SDL_Page& page);	// 페이지 추가
+	int AddPage(SDL_Page* page);	// 페이지 추가, 반환 값(페이지 id)
+	bool SelectPage(int pageID);	// 페이지 선택(전환)
+
+	void Refresh();					// 화면 새로고침
 
 private:
 	const char* title;
@@ -24,4 +27,9 @@ private:
 	bool bInitialized;
 
 	SDL_Surface *screen;
+
+	// for Pages
+	std::vector<SDL_Page*> pages;	// 페이지들
+	int curr_page;					// 선택된 페이지(시작은 0부터)
+
 };
