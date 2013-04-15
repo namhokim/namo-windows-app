54e96664460fd57f04902b5652eadf511868fce5
NHKimNamo@gmail.com <NHKimNamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2013-04-15 12:05:51 +0000

    다이얼로그 배치 및 이름 지정

--- trunk/trade/130306_toy_r_lang/TOY-R/TOY-R/Resource.h
+++ trunk/trade/130306_toy_r_lang/TOY-R/TOY-R/Resource.h
@@ -2,17 +2,28 @@
 // Microsoft Visual C++ generated include file.
 // Used by TOY-R.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_TOYR_DIALOG				102
+#define IDD_TOYR_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_CLEAR                1000
+#define IDC_BUTTON_LOAD_PROG            1001
+#define IDC_BUTTON_PREFIX               1002
+#define IDC_BUTTON_CONVERT              1003
+#define IDC_BUTTON_SAVE                 1004
+#define IDC_BUTTON_LOAD_IC              1005
+#define IDC_BUTTON_CALC                 1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON_QUIT                 1007
+#define IDC_EDIT_PROG                   1008
+#define IDC_EDIT_CONVERT                1009
+#define IDC_EDIT_RESULT                 1010
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
