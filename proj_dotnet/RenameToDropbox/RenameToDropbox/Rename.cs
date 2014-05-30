d3e916febc0eacc3ecd4951251aad510ce1444d7
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2014-05-30 02:54:55 +0000

    sCAM용 rename rule +

--- proj_dotnet/RenameToDropbox/RenameToDropbox/Rename.cs
+++ proj_dotnet/RenameToDropbox/RenameToDropbox/Rename.cs
@@ -6,49 +6,70 @@ namespace RenameToDropbox
 {
     public class Rename
     {
+        #region "static field"
         static readonly HashSet<String> extensions = new HashSet<String> { ".jpg", ".mp4" };
-        static readonly string MatchRule1 =
+        static readonly Regex MatchRule1 = new Regex(
             @"(?<YYYY>\d{4}?)(?<MM>\d{2}?)(?<DD>\d{2}?)_"
-            + @"(?<hh>\d{2}?)(?<mm>\d{2}?)(?<ss>\d{2}?)\((?<seq>\d+?)\)";
-        static readonly string MatchRule2 =
+            + @"(?<hh>\d{2}?)(?<mm>\d{2}?)(?<ss>\d{2}?)\((?<seq>(\d+)?)\)");
+        static readonly Regex MatchRule2 = new Regex(
             @"(?<YYYY>\d{4}?)(?<MM>\d{2}?)(?<DD>\d{2}?)_"
-            + @"(?<hh>\d{2}?)(?<mm>\d{2}?)(?<ss>\d{2}?)";
+            + @"(?<hh>\d{2}?)(?<mm>\d{2}?)(?<ss>\d{2}?)");
+        static readonly Regex MatchRuleSCAM = new Regex(
+            @"(?<MM>\d{2}?)-(?<DD>\d{2}?)@"
+            + @"(?<hh>\d{2}?)-(?<mm>\d{2}?)-(?<ss>\d{2}?)-(?<fff>(\d+)?)");
+        #endregion
 
         public static bool IsTargetExtension(string extension)
         {
             return extensions.Contains(extension);
         }
 
-        public static bool IsTargetPattern(string filename)
+        public static bool IsTargetPattern(string onlyFilename)
         {
-            Match match1 = Regex.Match(filename, MatchRule1);
-            Match match2 = Regex.Match(filename, MatchRule2);
-            return (match1.Success || match2.Success);
+            Match match1 = MatchRule1.Match(onlyFilename);
+            Match match2 = MatchRule2.Match(onlyFilename);
+            Match matchSCAM = MatchRuleSCAM.Match(onlyFilename);
+            return (match1.Success || match2.Success || matchSCAM.Success);
         }
 
         public static string GetNewName(string onlyFilename)
         {
-            Match match1 = Regex.Match(onlyFilename, MatchRule1);
-            Match match2 = Regex.Match(onlyFilename, MatchRule2);
+            Match match1 = MatchRule1.Match(onlyFilename);
+            Match match2 = MatchRule2.Match(onlyFilename);
+            Match matchSCAM = MatchRuleSCAM.Match(onlyFilename);
+            string returnValue = string.Empty;
+
             if (match1.Success)
             {
-                string newValue = string.Format("{0}-{1}-{2} {3}.{4}.{5} {6}",
-                    match1.Groups["YYYY"].Value, match1.Groups["MM"].Value, match1.Groups["DD"].Value,
-                    match1.Groups["hh"].Value, match1.Groups["mm"].Value, match1.Groups["ss"].Value,
-                    match1.Groups["seq"].Value);
-                return newValue;
+                Match match = match1;
+                returnValue = string.Format("{0}-{1}-{2} {3}.{4}.{5} {6}",
+                    match.Groups["YYYY"].Value, match.Groups["MM"].Value, match.Groups["DD"].Value,
+                    match.Groups["hh"].Value, match.Groups["mm"].Value, match.Groups["ss"].Value,
+                    match.Groups["seq"].Value);
             }
             else if (match2.Success)
             {
-                string newValue = string.Format("{0}-{1}-{2} {3}.{4}.{5}",
-                    match2.Groups["YYYY"].Value, match2.Groups["MM"].Value, match2.Groups["DD"].Value,
-                    match2.Groups["hh"].Value, match2.Groups["mm"].Value, match2.Groups["ss"].Value);
-                return newValue;
+                Match match = match2;
+                returnValue = string.Format("{0}-{1}-{2} {3}.{4}.{5}",
+                    match.Groups["YYYY"].Value, match.Groups["MM"].Value, match.Groups["DD"].Value,
+                    match.Groups["hh"].Value, match.Groups["mm"].Value, match.Groups["ss"].Value);
+                return returnValue;
+            }
+            else if (matchSCAM.Success)
+            {
+                Match match = matchSCAM;
+                returnValue = string.Format("{0}-{1}-{2} {3}.{4}.{5}.{6:000}",
+                    DateTime.Now.Year, match.Groups["MM"].Value, match.Groups["DD"].Value,
+                    match.Groups["hh"].Value, match.Groups["mm"].Value, match.Groups["ss"].Value,
+                    Int32.Parse(match.Groups["fff"].Value));
+                return returnValue;
             }
             else
             {
                 throw new ArgumentException("이름이 잘못되었습니다.");
             }
+
+            return returnValue;
         }
 
     }
