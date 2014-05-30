6a3b836b48a249c94803996d716869930627d2c3
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2014-05-30 02:54:55 +0000

    sCAM용 rename rule +

--- trunk/proj_dotnet/RenameToDropbox/RenameToDropbox.Tests.Unit/RenameTests.cs
+++ trunk/proj_dotnet/RenameToDropbox/RenameToDropbox.Tests.Unit/RenameTests.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace RenameToDropbox
 {
     using NUnit.Framework;
@@ -26,7 +27,11 @@ namespace RenameToDropbox
         [Test]
         public void TestIsTargetPattern_변환할형태파일명()
         {
-            Assert.True(Rename.IsTargetPattern("20140524_111014"));
+            Assert.True(Rename.IsTargetPattern("20140524_111014"), "기본 파일명");
+            Assert.True(Rename.IsTargetPattern("20140524_111014(0)"), "중복 파일명");
+            Assert.True(Rename.IsTargetPattern("05-02@11-07-32-7"), "sCAM 파일명(.f)");
+            Assert.True(Rename.IsTargetPattern("05-02@11-07-32-78"), "sCAM 파일명(.ff)");
+            Assert.True(Rename.IsTargetPattern("05-02@11-07-32-783"), "sCAM 파일명(.fff)");
         }
 
         [Test]
@@ -34,17 +39,29 @@ namespace RenameToDropbox
         {
             Assert.False(Rename.IsTargetPattern("2014-05-24 07.37.30"));
         }
+
         #endregion
 
         #region "GetNewName 메서드 테스트"
         [Test]
-        public void TestGetNewName_변환할형태파일명()
+        public void TestGetNewName_변환할형태파일명_원래카메라()
         {
             Assert.AreEqual("2014-05-24 11.10.14",
                 Rename.GetNewName("20140524_111014"));
 
             Assert.AreEqual("2014-05-24 11.10.14 0",
                 Rename.GetNewName("20140524_111014(0)"));
+        }
+
+        [Test]
+        public void TestGetNewName_변환할형태파일명_sCAM()
+        {
+            Assert.AreEqual(DateTime.Now.Year + "-05-02 11.07.32.007",
+                Rename.GetNewName("05-02@11-07-32-7"), "ms 1자리");
+            Assert.AreEqual(DateTime.Now.Year + "-05-02 11.07.32.072",
+                Rename.GetNewName("05-02@11-07-32-72"), "ms 2자리");
+            Assert.AreEqual(DateTime.Now.Year + "-05-02 11.07.32.723",
+                Rename.GetNewName("05-02@11-07-32-723"), "ms 3자리");
         }
         #endregion
 
---
+++ trunk/proj_dotnet/RenameToDropbox/RenameToDropbox.Tests.Unit/System/StringTests.cs
@@ -0,0 +1,19 @@
+﻿
+namespace System
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class StringTests
+    {
+        [Test]
+        public void TestFormat()
+        {
+            Assert.AreEqual("0.001", string.Format("0.{0:000}", 1));
+            Assert.AreEqual("0.002", string.Format("0.{0:000}", 2));
+            Assert.AreEqual("0.003", string.Format("0.{0:000}", 3));
+            Assert.AreEqual("0.010", string.Format("0.{0:000}", 10));
+            Assert.AreEqual("0.100", string.Format("0.{0:000}", 100));
+        }
+    }
+}
