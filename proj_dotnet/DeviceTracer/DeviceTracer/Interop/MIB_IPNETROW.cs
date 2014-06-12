46cd98aadc89802c02ce6b6913c04ec08b18118b
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2014-06-12 08:20:11 +0000

    Iphlpapi.GetIpNetTable wrapper implemented

---
+++ proj_dotnet/DeviceTracer/DeviceTracer/Interop/MIB_IPNETROW.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DeviceTracer.Interop
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MIB_IPNETROW
+    {
+        /// <summary>
+        /// The index of the adapter.
+        /// </summary>
+        public int dwIndex;
+        /// <summary>
+        /// The length, in bytes, of the physical address.
+        /// </summary>
+        public int dwPhysAddrLen;
+        /// <summary>
+        /// The physical address.
+        /// </summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = IPHelper.MAXLEN_PHYSADDR)]
+        public byte[] bPhysAddr;
+        /// <summary>
+        /// The IPv4 address.
+        /// </summary>
+        public int dwAddr;
+        /// <summary>
+        /// The type of ARP entry. This type can be one of the following values.
+        /// </summary>
+        public int dwType;
+    }
+}
---
+++ proj_dotnet/DeviceTracer/DeviceTracer/Interop/MibIpNetType.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DeviceTracer.Interop
+{
+    /// <summary>
+    /// enum MIB_IPNET_TYPE in ipmib.h L.133~138
+    /// </summary>
+    public enum MibIpNetType : int
+    {
+        Other = 1,
+        Invalid = 2,
+        Dynamic = 3,
+        Static = 4,
+    }
+}
