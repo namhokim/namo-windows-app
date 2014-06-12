9c077e221ecfc8a3f176bf9327790f77d9bd56b8
nhkimnamo@gmail.com <nhkimnamo@gmail.com@052871af-c2ae-dbb5-243f-d41ec41ef811>
2014-06-12 07:18:29 +0000

    ConsoleUtil - Run as admin +

---
+++ proj_dotnet/DeviceTracer/DeviceTracer.Tests.Integration/Util/ConsoleUtilTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeviceTracer.Util
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class ConsoleUtilTests
+    {
+        [Test]
+        public void IsRunAsAdminTest()
+        {
+            // 현재 NUnit이 관리자 모드가 아니라고 가정한다.
+            Assert.False(ConsoleUtil.IsRunAsAdmin());
+        }
+
+        [Test]
+        public void RunAsAdminTest()
+        {
+            ConsoleUtil.RunAsAdmin("arp", "-d *");
+
+        }
+    }
+}
---
+++ proj_dotnet/DeviceTracer/DeviceTracer/Util/ConsoleUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;    // for WindowsIdentity, WindowsPrincipal
+
+namespace DeviceTracer.Util
+{
+    public static class ConsoleUtil
+    {
+        /// <summary>
+        /// 관리자 권한으로 프로그램이 동작하는지 판단
+        /// </summary>
+        /// <returns>bool: true/false</returns>
+        public static bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static void RunAsAdmin(string fileName, string arguments)
+        {
+            // Launch itself as administrator
+            ProcessStartInfo proc = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                WorkingDirectory = Environment.CurrentDirectory,
+                FileName = fileName,
+                Arguments = arguments,
+                Verb = "runas",
+            };
+
+            Process.Start(proc);
+        }
+
+    }
+}
